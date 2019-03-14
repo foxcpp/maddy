5426bb4bebb897f2da7e2ead4050a40077d9901d
fox.cpp <fox.cpp@disroot.org>
2019-03-14 20:36:11 +0300

    Drop module.IMAPBackend, fix few typos, add module.WaitGroup
    IMAPBackend is not necessary, WaitGroup is useful for graceful shutdown.

--- module/registry.go
+++ module/registry.go
@@ -5,19 +5,27 @@ import "sync"
 var modules = make(map[string]NewModule)
 var modulesLck sync.RWMutex
 
+var ModuleDirectives []string
+
 // Register adds module factory function to global registry.
 //
 // name must be unique. Register will panic if module with specified name
 // already exists in registry.
 //
+// Contents of cfgDirectives will be appended ModuleDirectives. You should include
+// all used config directives here, otherwise configuration file will not be parsed
+// correctly.
+//
 // You probably want to call this function from func init() of module package.
-func Register(name string, factory NewModule) {
+func Register(name string, factory NewModule, cfgDirectives []string) {
 	modulesLck.Lock()
 	defer modulesLck.Unlock()
 
 	if _, ok := modules[name]; ok {
 		panic("Register: module with specified name is already registered: " + name)
 	}
+
+	ModuleDirectives = append(ModuleDirectives, cfgDirectives...)
 
 	modules[name] = factory
 }
@@ -55,7 +63,7 @@ func RegisterInstance(inst Module) {
 // GetInstance returns module instance from global registry.
 //
 // Nil is returned if no module instance with specified name is registered.
-func GetInstnace(name string) Module {
+func GetInstance(name string) Module {
 	instancesLck.RLock()
 	defer instancesLck.RUnlock()
 
--- module/storage.go
+++ module/storage.go
@@ -10,21 +10,3 @@ type Storage interface {
 	// If it doesn't exists - it should be created.
 	GetUser(username string) (imapbackend.User, error)
 }
-
-// IMAPBackend is structure that binds together implementations of Storage and
-// AuthProvider interfaces to get full go-imap Backend implementation.
-type IMAPBackend struct {
-	Auth  AuthProvider
-	Store Storage
-}
-
-// Force check to ensure that we really implement go-imap interface.
-var _ imapbackend.Backend = IMAPBackend{}
-
-func (be IMAPBackend) Login(username, password string) (imapbackend.User, error) {
-	if !be.Auth.CheckPlain(username, password) {
-		return nil, imapbackend.ErrInvalidCredentials
-	}
-
-	return be.Store.GetUser(username)
-}
