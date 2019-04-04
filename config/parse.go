64ef51ce23dedf8c8078ae8cf9be90451f1bbced
fox.cpp <fox.cpp@disroot.org>
2019-04-04 23:38:43 +0300

    config: Fix typos in parse.go documentation + change error description

--- config/parse.go
+++ config/parse.go
@@ -9,24 +9,31 @@ import (
 	"github.com/mholt/caddy/caddyfile"
 )
 
+// Node struct describes a parsed configurtion block or a simple directive.
+//
 // name arg0 arg1 {
 //  children0
 //  children1
 // }
 type Node struct {
-	// First string at node's line.
+	// Name is the first string at node's line.
 	Name string
-	// Any strings placed after node name.
+	// Args are any strings placed after the node name.
 	Args []string
 
-	// If node is a block - all nodes inside the block. Can be nil.
+	// Children slice contains all children blocks if node is a block. Can be nil.
 	Children []Node
 
-	// Whether current parsed node is a snippet. Always false for all nodes
-	// returned from Read because snippets are expanded before it returns.
+	// Snippet indicates whether current parsed node is a snippet. Always false
+	// for all nodes returned from Read because snippets are expanded before it
+	// returns.
 	Snippet bool
 
+	// File is the name of node's source file.
 	File string
+
+	// Line is the line number where the directive is located in the source file. For
+	// blocks this is the line where "block header" (name + args) resides.
 	Line int
 }
 
@@ -38,17 +45,16 @@ type parseContext struct {
 	fileLocation string
 }
 
-// readNode reads node starting at current token pointed by lexer's
+// readNode reads node starting at current token pointed by the lexer's
 // cursor (it should point to node name).
 //
-// After readNode returns lexer's cursor will point to last token of parsed
-// Node. This ensures predicatable cursor location independently of EOF state.
+// After readNode returns, the lexer's cursor will point to the last token of the parsed
+// Node. This ensures predictable cursor location independently of the EOF state.
 // Thus code reading multiple nodes should call readNode then manually
 // advance lexer cursor (ctx.Next) and either call readNode again or stop
 // because cursor hit EOF.
 //
 // readNode calls readNodes if currently parsed node is a block.
-// ctx.nesting is incremented call.
 func (ctx *parseContext) readNode() (Node, error) {
 	node := Node{}
 	node.File = ctx.File()
@@ -96,15 +102,15 @@ func (ctx *parseContext) isSnippet(name string) (bool, string) {
 	return false, ""
 }
 
-// readNodes reads nodes from currently parsed block.
+// readNodes reads nodes from the currently parsed block.
 //
-// Lexer's cursor should point to opening brace
+// The lexer's cursor should point to the opening brace
 // name arg0 arg1 {  #< this one
 //   c0
 //   c1
 // }
 //
-// To stay consistent with readNode after this function returns lexer's cursor pointer
+// To stay consistent with readNode after this function returns the lexer's cursor points
 // to the last token of the black (closing brace).
 func (ctx *parseContext) readNodes() ([]Node, error) {
 	res := []Node{}
@@ -122,7 +128,7 @@ func (ctx *parseContext) readNodes() ([]Node, error) {
 		//   c1
 		// }
 		// ^ called when we hit } on separate line,
-		// This means block we are supposed to parse ended and we should stop.
+		// This means block we hit end of our block.
 		if ctx.Val() == "}" {
 			ctx.nesting--
 			// name arg0 arg1 { #<1
@@ -131,7 +137,7 @@ func (ctx *parseContext) readNodes() ([]Node, error) {
 			//
 			// After #1 ctx.nesting is incremented by ctx.nesting++ before this loop.
 			// Then we advance cursor and hit }, we exit loop, ctx.nesting now becomes 0.
-			// But then parent block reader does the same when it hits #3 -
+			// But then the parent block reader does the same when it hits #3 -
 			// ctx.nesting becomes -1 and it fails.
 			if ctx.nesting < 0 {
 				return res, ctx.Err("unexpected }")
@@ -148,8 +154,8 @@ func (ctx *parseContext) readNodes() ([]Node, error) {
 		// name arg0 arg1 {
 		//   c1 c2 }
 		//         ^
-		// Edgy case, here we check if last argument of last node is a }
-		// If it is - we stop as we hit end of our block.
+		// Edge case, here we check if the last argument of the last node is a }
+		// If it is - we stop as we hit the end of our block.
 		if len(node.Args) != 0 && node.Args[len(node.Args)-1] == "}" {
 			ctx.nesting--
 			if ctx.nesting < 0 {
@@ -190,9 +196,9 @@ func readTree(r io.Reader, location string, expansionDepth int) (nodes []Node, s
 	root := Node{}
 	root.File = location
 	root.Line = 1
-	// Before parsing starts lexer's cursor points to the non-existent token before
-	// first one. From readNodes viewpoint this is opening brace so we
-	// don't break any requirements here.
+	// Before parsing starts the lexer's cursor points to the non-existent
+	// token before the first one. From readNodes viewpoint this is opening
+	// brace so we don't break any requirements here.
 	//
 	// For the same reason we use -1 as a starting nesting. So readNodes
 	// will see this as it is reading block at nesting level 0.
@@ -203,7 +209,7 @@ func readTree(r io.Reader, location string, expansionDepth int) (nodes []Node, s
 
 	// There is no need to check ctx.nesting < 0 because it is checked by readNodes.
 	if ctx.nesting > 0 {
-		return root.Children, ctx.snippets, ctx.Err("unexpected {")
+		return root.Children, ctx.snippets, ctx.Err("unexpected EOF when looking for }")
 	}
 
 	if err := ctx.expandImports(&root, expansionDepth); err != nil {
