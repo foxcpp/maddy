e4b328cc07d378f14140c8d161b8e6d0efb39043
fox.cpp <fox.cpp@disroot.org>
2019-05-26 19:25:58 +0300

    mtasts: Add function to refresh entire cache

--- mtasts/cache.go
+++ mtasts/cache.go
@@ -3,12 +3,15 @@ package mtasts
 import (
 	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/emersion/maddy/log"
 )
 
 func downloadPolicy(domain string) (*Policy, error) {
@@ -37,68 +40,15 @@ func downloadPolicy(domain string) (*Policy, error) {
 // directory.
 type Cache struct {
 	Location string
+	Logger   *log.Logger
 }
 
 var ErrNoPolicy = errors.New("mtasts: no MTA-STS policy found")
 
 // Get reads policy from cache or tries to fetch it from Policy Host.
 func (c *Cache) Get(domain string) (*Policy, error) {
-	validCache := true
-	cachedId, fetchTime, cachedPolicy, err := c.load(domain)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			// Something wrong with FS directory used for caching, this is bad.
-			return nil, err
-		}
-
-		validCache = false
-	} else if fetchTime.Add(time.Duration(cachedPolicy.MaxAge) * time.Second).Before(time.Now()) {
-		validCache = false
-	}
-
-	records, err := net.LookupTXT("_mta-sts." + domain)
-	if err != nil {
-		if derr, ok := err.(*net.DNSError); ok && !derr.IsTemporary {
-			return nil, ErrNoPolicy
-		}
-		return nil, err
-	}
-
-	// RFC says:
-	//   If the number of resulting records is not one, or if the resulting
-	//   record is syntactically invalid, senders MUST assume the recipient
-	//   domain does not have an available MTA-STS Policy. ...
-	//   (Note that the absence of a usable TXT record is not by itself
-	//   sufficient to remove a sender's previously cached policy for the Policy
-	//   Domain, as discussed in Section 5.1, "Policy Application Control Flow".)
-	if len(records) != 1 {
-		if validCache {
-			return cachedPolicy, nil
-		}
-		return nil, ErrNoPolicy
-	}
-	dnsId, err := readDNSRecord(records[0])
-	if err != nil {
-		if validCache {
-			return cachedPolicy, nil
-		}
-		return nil, ErrNoPolicy
-	}
-
-	if !validCache || dnsId != cachedId {
-		policy, err := downloadPolicy(domain)
-		if err != nil {
-			return nil, err
-		}
-
-		if err := c.store(domain, dnsId, time.Now(), policy); err != nil {
-			// We still got up-to-date policy, cache is not critcial.
-			return policy, nil
-		}
-		return policy, nil
-	}
-
-	return cachedPolicy, nil
+	_, p, err := c.fetch(domain)
+	return p, err
 }
 
 func (c *Cache) store(domain, id string, fetchTime time.Time, p *Policy) error {
@@ -131,4 +81,94 @@ func (c *Cache) load(domain string) (id string, fetchTime time.Time, p *Policy, 
 		return "", time.Time{}, nil, err
 	}
 	return data.ID, data.FetchTime, data.Policy, nil
+}
+
+func (c *Cache) RefreshCache() error {
+	dir, err := ioutil.ReadDir(c.Location)
+	if err != nil {
+		return err
+	}
+
+	for _, ent := range dir {
+		if ent.IsDir() {
+			continue
+		}
+		cacheHit, _, err := c.fetch(ent.Name())
+		if err != nil {
+			c.Logger.Printf("failed to update MTA-STS policy for %v: %v", ent.Name(), err)
+		}
+		if !cacheHit && err == nil {
+			c.Logger.Debugln("updated MTA-STS policy for", ent.Name())
+		}
+
+		// This means cached version is expired and remote offers no updated policy.
+		// Remove cached version to save space.
+		if !cacheHit && err == ErrNoPolicy {
+			if err := os.Remove(filepath.Join(c.Location, ent.Name())); err != nil {
+				c.Logger.Println("failed to remove MTA-STS policy for", ent.Name())
+			}
+			c.Logger.Debugln("removed MTA-STS policy for", ent.Name())
+		}
+	}
+
+	return nil
+}
+
+func (c *Cache) fetch(domain string) (cacheHit bool, p *Policy, err error) {
+	validCache := true
+	cachedId, fetchTime, cachedPolicy, err := c.load(domain)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			// Something wrong with FS directory used for caching, this is bad.
+			return false, nil, err
+		}
+
+		validCache = false
+	} else if fetchTime.Add(time.Duration(cachedPolicy.MaxAge) * time.Second).Before(time.Now()) {
+		validCache = false
+	}
+
+	records, err := net.LookupTXT("_mta-sts." + domain)
+	if err != nil {
+		if derr, ok := err.(*net.DNSError); ok && !derr.IsTemporary {
+			return false, nil, ErrNoPolicy
+		}
+		return false, nil, err
+	}
+
+	// RFC says:
+	//   If the number of resulting records is not one, or if the resulting
+	//   record is syntactically invalid, senders MUST assume the recipient
+	//   domain does not have an available MTA-STS Policy. ...
+	//   (Note that the absence of a usable TXT record is not by itself
+	//   sufficient to remove a sender's previously cached policy for the Policy
+	//   Domain, as discussed in Section 5.1, "Policy Application Control Flow".)
+	if len(records) != 1 {
+		if validCache {
+			return true, cachedPolicy, nil
+		}
+		return false, nil, ErrNoPolicy
+	}
+	dnsId, err := readDNSRecord(records[0])
+	if err != nil {
+		if validCache {
+			return true, cachedPolicy, nil
+		}
+		return false, nil, ErrNoPolicy
+	}
+
+	if !validCache || dnsId != cachedId {
+		policy, err := downloadPolicy(domain)
+		if err != nil {
+			return false, nil, err
+		}
+
+		if err := c.store(domain, dnsId, time.Now(), policy); err != nil {
+			// We still got up-to-date policy, cache is not critcial.
+			return false, cachedPolicy, nil
+		}
+		return false, policy, nil
+	}
+
+	return true, cachedPolicy, nil
 }
