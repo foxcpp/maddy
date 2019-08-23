c25b3e3362e97b3e1fb81aff782b6b2aa3ff8b00
fox.cpp <fox.cpp@disroot.org>
2019-08-24 00:38:24 +0300

    Update DNS checks to use statelessCheck wrapper

--- envelope_checks.go
+++ envelope_checks.go
@@ -3,66 +3,68 @@ package maddy
 import (
 	"context"
 	"errors"
-	"io"
 	"net"
 	"strings"
 
 	"github.com/emersion/maddy/log"
-	"github.com/emersion/maddy/module"
 )
 
-func checkSrcRDNS(r Resolver, ctx *module.DeliveryContext, _ io.Reader) (io.Reader, error) {
-	tcpAddr, ok := ctx.SrcAddr.(*net.TCPAddr)
+func checkSrcRDNS(ctx StatelessCheckContext) error {
+	tcpAddr, ok := ctx.DeliveryCtx.SrcAddr.(*net.TCPAddr)
 	if !ok {
-		log.Debugf("check_source_rdns: non TCP/IP source (%v), skipped", ctx.SrcAddr)
-		return nil, nil
+		log.Debugf("non TCP/IP source (%v), skipped", ctx.DeliveryCtx.SrcAddr)
+		return nil
 	}
 
-	names, err := r.LookupAddr(context.Background(), tcpAddr.IP.String())
+	names, err := ctx.Resolver.LookupAddr(context.Background(), tcpAddr.IP.String())
 	if err != nil || len(names) == 0 {
-		log.Printf("check_source_rdns: rDNS query for %v failed (%v), FAIL, delivery ID = %s", tcpAddr.IP, err, ctx.DeliveryID)
-		return nil, errors.New("could look up rDNS address for source")
+		log.Printf("rDNS query for %v failed: %v", tcpAddr.IP, err)
+		return errors.New("could look up rDNS address for source")
 	}
 
-	srcDomain := strings.TrimRight(ctx.SrcHostname, ".")
+	srcDomain := strings.TrimRight(ctx.DeliveryCtx.SrcHostname, ".")
 
 	for _, name := range names {
 		if strings.TrimRight(name, ".") == srcDomain {
-			log.Debugf("check_source_rdns: PTR record %s matches source domain, OK, delivery ID = %s", name, ctx.DeliveryID)
-			return nil, nil
+			ctx.Logger.Debugf("PTR record %s matches source domain, OK", name)
+			return nil
 		}
 	}
-	log.Printf("check_source_rdns: no PTR records for %v IP pointing to %s, FAIL, delivery ID = %s", tcpAddr.IP, srcDomain, ctx.DeliveryID)
-	return nil, errors.New("rDNS name does not match source hostname")
+	ctx.Logger.Printf("no PTR records for %v IP pointing to %s", tcpAddr.IP, srcDomain)
+	// TODO: Use SMTPError with enhanced codes. Also for remaining checks.
+	return errors.New("rDNS name does not match source hostname")
 }
 
-func checkSrcMX(r Resolver, ctx *module.DeliveryContext, _ io.Reader) (io.Reader, error) {
-	parts := strings.Split(ctx.From, "@")
-	if len(parts) != 2 {
-		return nil, errors.New("malformed address")
-	}
-	domain := parts[1]
-
-	tcpAddr, ok := ctx.SrcAddr.(*net.TCPAddr)
-	if !ok {
-		log.Debugf("check_source_mx: not TCP/IP source (%v), skipped", ctx.SrcAddr)
-		return nil, nil
-	}
-
-	srcMx, err := r.LookupMX(context.Background(), domain)
+func checkSrcMX(ctx StatelessCheckContext, mailFrom string) error {
+	_, domain, err := splitAddress(mailFrom)
 	if err != nil {
-		log.Debugf("check_source_mx: %s does not resolve, FAIL, delivery ID = %s", domain, ctx.DeliveryID)
-		return nil, errors.New("could not find MX records for from domain")
+		return err
+	}
+	if domain == "" {
+		// TODO: Make it configurable whether <postmaster> is allowed.
+		return errors.New("check_source_mx: <postmaster> is not allowed")
+	}
+
+	tcpAddr, ok := ctx.DeliveryCtx.SrcAddr.(*net.TCPAddr)
+	if !ok {
+		ctx.Logger.Debugf("not TCP/IP source (%v), skipped", ctx.DeliveryCtx.SrcAddr)
+		return nil
+	}
+
+	srcMx, err := ctx.Resolver.LookupMX(context.Background(), domain)
+	if err != nil {
+		ctx.Logger.Debugf("%s does not resolve", domain)
+		return errors.New("could not find MX records for from domain")
 	}
 
 	for _, mx := range srcMx {
-		if mx.Host == ctx.SrcHostname || mx.Host == tcpAddr.IP.String() {
-			log.Debugf("check_source_mx: MX record %s matches %v (%v), OK, delivery ID = %s", mx.Host, ctx.SrcHostname, tcpAddr.IP, ctx.DeliveryID)
-			return nil, nil
+		if mx.Host == ctx.DeliveryCtx.SrcHostname || mx.Host == tcpAddr.IP.String() {
+			ctx.Logger.Debugf("MX record %s matches %v (%v)", mx.Host, ctx.DeliveryCtx.SrcHostname, tcpAddr.IP)
+			return nil
 		}
 
 		// MX record may contain hostname that will resolve to source hostname, check that too.
-		addrs, err := r.LookupIPAddr(context.Background(), mx.Host)
+		addrs, err := ctx.Resolver.LookupIPAddr(context.Background(), mx.Host)
 		if err != nil {
 			// TODO: Actually check whether mx.Host is a domain.
 			// Probably that was an IP.
@@ -71,36 +73,40 @@ func checkSrcMX(r Resolver, ctx *module.DeliveryContext, _ io.Reader) (io.Reader
 
 		for _, addr := range addrs {
 			if addr.IP.Equal(tcpAddr.IP) {
-				log.Debugf("check_source_mx: MX record %s matches %v (%v), OK, delivery ID = %s", mx.Host, ctx.SrcHostname, tcpAddr.IP, ctx.DeliveryID)
-				return nil, nil
+				ctx.Logger.Debugf("MX record %s matches %v (%v), OK", mx.Host, ctx.DeliveryCtx.SrcHostname, tcpAddr.IP)
+				return nil
 			}
 		}
 	}
-	log.Printf("check_source_mx: no matching MX records for %s (%s), FAIL, delivery ID = %s", ctx.SrcHostname, tcpAddr.IP, ctx.DeliveryID)
-	return nil, errors.New("From domain has no MX record for itself")
+	ctx.Logger.Printf("no matching MX records for %s (%s)", ctx.DeliveryCtx.SrcHostname, tcpAddr.IP)
+	return errors.New("domain in MAIL FROM has no MX record for itself")
 }
 
-func checkSrcHostname(r Resolver, ctx *module.DeliveryContext, _ io.Reader) (io.Reader, error) {
-	tcpAddr, ok := ctx.SrcAddr.(*net.TCPAddr)
+func checkSrcHostname(ctx StatelessCheckContext) error {
+	tcpAddr, ok := ctx.DeliveryCtx.SrcAddr.(*net.TCPAddr)
 	if !ok {
-		log.Debugf("check_source_hostname: not TCP/IP source (%v), skipped", ctx.SrcAddr)
-		return nil, nil
+		ctx.Logger.Debugf("not TCP/IP source (%v), skipped", ctx.DeliveryCtx.SrcAddr)
+		return nil
 	}
 
-	srcIPs, err := r.LookupIPAddr(context.Background(), ctx.SrcHostname)
+	srcIPs, err := ctx.Resolver.LookupIPAddr(context.Background(), ctx.DeliveryCtx.SrcHostname)
 	if err != nil {
-		log.Printf("check_source_hostname: %s does not resolve, FAIL, delivery ID = %s", ctx.SrcHostname, ctx.DeliveryID)
-		return nil, errors.New("source hostname does not resolve")
+		log.Printf("%s does not resolve", ctx.DeliveryCtx.SrcHostname)
+		return errors.New("source hostname does not resolve")
 	}
 
 	for _, ip := range srcIPs {
 		if tcpAddr.IP.Equal(ip.IP) {
-			log.Debugf("check_source_hostname: A/AAA record found for %s for %s domain, OK, delivery ID = %s", tcpAddr.IP, ctx.SrcHostname, ctx.DeliveryID)
-			return nil, nil
+			ctx.Logger.Debugf("A/AAA record found for %s for %s domain", tcpAddr.IP, ctx.DeliveryCtx.SrcHostname)
+			return nil
 		}
 	}
-	log.Printf("check_source_hostname: no A/AAA records found for %s for %s domain, FAIL, delivery ID = %s", tcpAddr.IP, ctx.SrcHostname, ctx.DeliveryID)
-	return nil, errors.New("source hostname does not resolve to source address")
+	ctx.Logger.Printf("no A/AAA records found for %s for %s domain", tcpAddr.IP, ctx.DeliveryCtx.SrcHostname)
+	return errors.New("source hostname does not resolve to source address")
 }
 
-// FIXME: Register
+func init() {
+	RegisterStatelessCheck("check_source_rdns", checkSrcRDNS, nil, nil, nil)
+	RegisterStatelessCheck("check_source_mx", nil, checkSrcMX, nil, nil)
+	RegisterStatelessCheck("check_source_hostname", checkSrcHostname, nil, nil, nil)
+}
