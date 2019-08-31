38465f7308d8971fe4b8dfc2286fa10118d25f63
fox.cpp <fox.cpp@disroot.org>
2019-09-01 01:34:51 +0300

    cmd/maddy: Allow to enable pprof HTTP endpoint
    This is a very useful tool for performance analysis, having it as hand
    without the need to always modify the source code to enable it is simply
    convenient.
    
    Also now we can ask users to use it when reporting performance bugs.

--- cmd/maddy/main.go
+++ cmd/maddy/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"path/filepath"
+	"runtime"
 
 	"github.com/foxcpp/maddy"
 	"github.com/foxcpp/maddy/config"
@@ -11,16 +14,34 @@ import (
 )
 
 func main() {
-	var configpath string
-	flag.StringVar(&configpath, "config", filepath.Join(maddy.ConfigDirectory(), "maddy.conf"), "path to configuration file")
+	configPath := flag.String("config", filepath.Join(maddy.ConfigDirectory(), "maddy.conf"), "path to configuration file")
 	debugFlag := flag.Bool("debug", false, "enable debug logging early")
+	profileEndpoint := flag.String("debug.pprof", "", "enable live profiler HTTP endpoint and listen on the specified endpoint")
+	blockProfileRate := flag.Int("debug.blockprofrate", 0, "set blocking profile rate")
+	mutexProfileFract := flag.Int("debug.mutexproffract", 0, "set mutex profile fraction")
 	flag.Parse()
+
+	if *profileEndpoint != "" {
+		go func() {
+			log.Println("listening on", "http://"+*profileEndpoint, "for profiler requests")
+			log.Println("failed to listen on profiler endpoint:", http.ListenAndServe(*profileEndpoint, nil))
+		}()
+	}
+
+	// These values can also be affected by environment so set them
+	// only if argument is specified.
+	if *mutexProfileFract != 0 {
+		runtime.SetMutexProfileFraction(*mutexProfileFract)
+	}
+	if *blockProfileRate != 0 {
+		runtime.SetBlockProfileRate(*blockProfileRate)
+	}
 
 	if *debugFlag {
 		log.DefaultLogger.Debug = true
 	}
 
-	absCfg, err := filepath.Abs(configpath)
+	absCfg, err := filepath.Abs(*configPath)
 	if err != nil {
 		log.Printf("failed to resolve path to config: %v\n", err)
 		os.Exit(1)
@@ -28,14 +49,14 @@ func main() {
 
 	f, err := os.Open(absCfg)
 	if err != nil {
-		log.Printf("cannot open %q: %v\n", configpath, err)
+		log.Printf("cannot open %q: %v\n", *configPath, err)
 		os.Exit(1)
 	}
 	defer f.Close()
 
 	cfg, err := config.Read(f, absCfg)
 	if err != nil {
-		log.Printf("cannot parse %q: %v\n", configpath, err)
+		log.Printf("cannot parse %q: %v\n", *configPath, err)
 		os.Exit(1)
 	}
 
