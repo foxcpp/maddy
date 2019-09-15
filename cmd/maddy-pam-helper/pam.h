3d60bfa2dbb82bd1b50f51d879694a93995f72d1
fox.cpp <fox.cpp@disroot.org>
2019-09-15 04:13:38 +0300

    cmd/maddy-pam-helper: Copy pam.c from auth/pam
    It needs to be slightly alerted (no build constraint).

--- cmd/maddy-pam-helper/pam.h
+++ cmd/maddy-pam-helper/pam.h
@@ -1,1 +1,9 @@
-../../auth/pam/pam.h
+#pragma once
+
+struct error_obj {
+    int status;
+    const char* func_name;
+    const char* error_msg;
+};
+
+struct error_obj run_pam_auth(const char *username, char *password);
