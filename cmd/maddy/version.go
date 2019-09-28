52a8a1e0327f70b5a28334e838f912e18bd167a9
fox.cpp <fox.cpp@disroot.org>
2019-09-28 13:35:14 +0300

    cmd/maddy: Remove fallbacks for printBuildInfo on Go 1.11
    We require Go 1.13 now.

--- cmd/maddy/version.go
+++ cmd/maddy/version.go
@@ -1,3 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"runtime/debug"
+)
+
 const Version = "unknown (built from source tree)"
+
+func printBuildInfo() {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version == "(devel)" {
+			fmt.Println("maddy", Version)
+		} else {
+			fmt.Println("maddy", info.Main.Version, info.Main.Sum)
+		}
+	} else {
+		fmt.Println("maddy", Version, "(GOPATH build)")
+		fmt.Println()
+		fmt.Println("Building maddy in GOPATH mode can lead to wrong dependency")
+		fmt.Println("versions being used. Problems created by this will not be")
+		fmt.Println("addressed. Make sure you are building in Module Mode")
+		fmt.Println("(see README for details)")
+	}
+}
