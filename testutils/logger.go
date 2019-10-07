46f5bcf6a4c8918416f0e8f25dd1e316c0144c4f
fox.cpp <fox.cpp@disroot.org>
2019-10-07 23:40:22 +0300

    testutils: Don't blackhole log messages without -v
    They can still be printed on test failure.

--- testutils/logger.go
+++ testutils/logger.go
@@ -9,23 +9,18 @@ import (
 )
 
 func Logger(t *testing.T, name string) log.Logger {
-	if testing.Verbose() {
-		return log.Logger{
-			Out: log.FuncOutput(func(_ time.Time, debug bool, str string) {
-				t.Helper()
-				str = strings.TrimSuffix(str, "\n")
-				if debug {
-					str = "[debug] " + str
-				}
-				t.Log(str)
-			}, func() error {
-				return nil
-			}),
-			Name:  name,
-			Debug: true,
-		}
+	return log.Logger{
+		Out: log.FuncOutput(func(_ time.Time, debug bool, str string) {
+			t.Helper()
+			str = strings.TrimSuffix(str, "\n")
+			if debug {
+				str = "[debug] " + str
+			}
+			t.Log(str)
+		}, func() error {
+			return nil
+		}),
+		Name:  name,
+		Debug: true,
 	}
-
-	// MultiOutput to empty slice is a blackhole.
-	return log.Logger{Out: log.MultiOutput()}
 }
