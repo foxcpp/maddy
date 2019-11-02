0eee6965204dc93c985121cd523364e79f7c1d0c
fox.cpp <fox.cpp@disroot.org>
2019-11-02 04:22:18 +0300

    msgpipeline: Shorten message IDs to 8 characters
    2^32 (given 4 octets for unencoded ID) is a good enough namespace to
    guarantee uniqueness within a small server.

--- msgpipeline/msgid.go
+++ msgpipeline/msgid.go
@@ -10,7 +10,7 @@ import (
 // TODO: Find a better place for this function. 'msgpipeline' package seems
 // irrelevant.
 func GenerateMsgID() (string, error) {
-	rawID := make([]byte, 32)
+	rawID := make([]byte, 4)
 	_, err := rand.Read(rawID)
 	return hex.EncodeToString(rawID), err
 }
