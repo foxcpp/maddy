2aa8b0ce5bc12eed336158fad00f36c9708617d3
fox.cpp <fox.cpp@disroot.org>
2019-11-09 12:53:34 +0300

    get.sh: Split into functions, allow to override sudo execution
    In case of network failure on curl|bash, bash can run only part of the
    script. This commit splits get.sh into a set of functions (for
    maintenance purposes) and adds run() function that actually runs all
    installation steps. The run() function is then called in the last line
    of the script, ensuring that if it is not downloaded completely, nothing
    will be done.
    
    Second change is to allow to replace sudo binary path using $SUDO
    envvar. This is meant for installations that replace
    all other paths with non-system directories so additional priveileges
    are not needed.

--- get.sh
+++ get.sh
@@ -9,9 +9,14 @@ fi
 if [ "$PREFIX" == "" ]; then
     PREFIX=/usr/local
 fi
-SYSTEMDUNITS=$PREFIX/lib/systemd
+if [ "$SYSTEMDUNITS" == "" ]; then
+    SYSTEMDUNITS=$PREFIX/lib/systemd
+fi
 if [ "$CONFPATH" == "" ]; then
     CONFPATH=/etc/maddy/maddy.conf
+fi
+if [ "$SUDO" == ""]; then
+    SUDO=$SUDO
 fi
 
 export CGO_CFLAGS="-g -O2 -D_FORTIFY_SOURCE=2 $CFLAGS"
@@ -22,102 +27,124 @@ export CGO_LDFLAGS=$LDFLAGS
 set -euo pipefail
 IFS=$'\n'
 
-mkdir -p maddy-setup/
-cd maddy-setup/
+ensure_go_toolchain() {
+    if ! which go >/dev/null; then
+        download=1
+    else
+        SYSGOVERSION=`go version | grep -Po "([0-9]+\.){2}[0-9]+"`
+        SYSGOMAJOR=`cut -f1 -d. <<<$SYSGOVERSION`
+        SYSGOMINOR=`cut -f2 -d. <<<$SYSGOVERSION`
+        SYSGOPATCH=`cut -f3 -d. <<<$SYSGOVERSION`
+        WANTEDGOMAJOR=`cut -f1 -d. <<<$GOVERSION`
+        WANTEDGOMINOR=`cut -f2 -d. <<<$GOVERSION`
+        WANTEDGOPATCH=`cut -f3 -d. <<<$GOVERSION`
 
-if ! which go >/dev/null; then
-    download=1
-else
-    SYSGOVERSION=`go version | grep -Po "([0-9]+\.){2}[0-9]+"`
-    SYSGOMAJOR=`cut -f1 -d. <<<$SYSGOVERSION`
-    SYSGOMINOR=`cut -f2 -d. <<<$SYSGOVERSION`
-    SYSGOPATCH=`cut -f3 -d. <<<$SYSGOVERSION`
-    WANTEDGOMAJOR=`cut -f1 -d. <<<$GOVERSION`
-    WANTEDGOMINOR=`cut -f2 -d. <<<$GOVERSION`
-    WANTEDGOPATCH=`cut -f3 -d. <<<$GOVERSION`
-
-    downloadgo=0
-    if [ $SYSGOMAJOR -ne $WANTEDGOMAJOR ]; then
-        downloadgo=1
-    fi
-    if [ $SYSGOMINOR -lt $WANTEDGOMINOR ]; then
-        downloadgo=1
-    fi
-    if [ $SYSGOPATCH -lt $WANTEDGOPATCH ]; then
-        downloadgo=1
-    fi
-
-    if [ $downloadgo -eq 0 ]; then
-        echo "Using system Go toolchain ($SYSGOVERSION, `which go`)." >&2
-    fi
-fi
-
-if [ $downloadgo -eq 1 ]; then
-    echo "Downloading Go $GOVERSION toolchain..." >&2
-    if ! [ -e go$GOVERSION ]; then
-        if ! [ -e go$GOVERSION.linux-amd64.tar.gz ]; then
-            wget -q 'https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz'
+        downloadgo=0
+        if [ $SYSGOMAJOR -ne $WANTEDGOMAJOR ]; then
+            downloadgo=1
         fi
-        tar xf go$GOVERSION.linux-amd64.tar.gz
-        mv go go$GOVERSION
+        if [ $SYSGOMINOR -lt $WANTEDGOMINOR ]; then
+            downloadgo=1
+        fi
+        if [ $SYSGOPATCH -lt $WANTEDGOPATCH ]; then
+            downloadgo=1
+        fi
+
+        if [ $downloadgo -eq 0 ]; then
+            echo "Using system Go toolchain ($SYSGOVERSION, `which go`)." >&2
+        fi
     fi
-    export GOROOT=$PWD/go$GOVERSION
-    export PATH=go$GOVERSION/bin:$PATH
-fi
 
-
-export GOPATH="$PWD/gopath"
-export GOBIN="$GOPATH/bin"
-
-echo 'Downloading and compiling maddy...' >&2
-
-export GO111MODULE=on
-
-go get -trimpath -buildmode=pie -ldflags "-extldflags $LDFLAGS" github.com/foxcpp/maddy/cmd/{maddy,maddyctl}@$MADDYVERSION
-
-echo 'Installing maddy...' >&2
-
-sudo mkdir -p "$PREFIX/bin"
-sudo cp --remove-destination "$GOPATH/bin/maddy" "$GOPATH/bin/maddyctl" "$PREFIX/bin/"
-
-echo 'Downloading and installing systemd unit files...' >&2
-
-wget -q "https://raw.githubusercontent.com/foxcpp/maddy/$MADDYVERSION/dist/systemd/maddy.service" -O maddy.service
-wget -q "https://raw.githubusercontent.com/foxcpp/maddy/$MADDYVERSION/dist/systemd/maddy@.service" -O maddy@.service
-
-sed -Ei "s!/usr/bin!$PREFIX/bin!g" maddy.service maddy@.service
-
-sudo mkdir -p "$SYSTEMDUNITS/system/"
-sudo cp maddy.service maddy@.service "$SYSTEMDUNITS/system/"
-sudo systemctl daemon-reload
-
-echo 'Creating maddy user and group...' >&2
-
-sudo useradd -UMr -s /sbin/nologin maddy || true
-
-echo 'Using configuration path:' $CONFPATH
-if ! [ -e "$CONFPATH" ]; then
-    echo 'Downloading and installing default configuration...' >&2
-
-    wget -q "https://raw.githubusercontent.com/foxcpp/maddy/$MADDYVERSION/maddy.conf" -O maddy.conf
-    sudo mkdir -p /etc/maddy/
-
-    host=`hostname`
-    read -p "What's your domain, btw? [$host] > " DOMAIN
-    if [ "$DOMAIN" = "" ]; then
-        DOMAIN=$host
+    if [ $downloadgo -eq 1 ]; then
+        echo "Downloading Go $GOVERSION toolchain..." >&2
+        if ! [ -e go$GOVERSION ]; then
+            if ! [ -e go$GOVERSION.linux-amd64.tar.gz ]; then
+                wget -q 'https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz'
+            fi
+            tar xf go$GOVERSION.linux-amd64.tar.gz
+            mv go go$GOVERSION
+        fi
+        export GOROOT=$PWD/go$GOVERSION
+        export PATH=go$GOVERSION/bin:$PATH
     fi
-    echo 'Good, I will put that into configuration for you.' >&2
+}
 
-    sed -Ei "s/^\\$\\(primary_domain\) = .+$/$\(primary_domain\) = $DOMAIN/" maddy.conf
-    sed -Ei "s/^\\$\\(hostname\) = .+$/$\(hostname\) = $DOMAIN/" maddy.conf
+download_and_compile() {
+    export GOPATH="$PWD/gopath"
+    export GOBIN="$GOPATH/bin"
 
-    sudo cp maddy.conf /etc/maddy/
-else
-    echo "Configuration already exists in /etc/maddy/maddy.conf, skipping defaults installation." >&2
-fi
+    echo 'Downloading and compiling maddy...' >&2
 
-echo "Okay, almost ready." >&2
-echo "It's up to you to figure out TLS certificates and DNS stuff, though." >&2
-echo "Here is the tutorial to help you:" >&2
-echo "https://github.com/foxcpp/maddy/wiki/Tutorial:-Setting-up-a-mail-server-with-maddy" >&2
+    export GO111MODULE=on
+
+    go get -trimpath -buildmode=pie -ldflags "-extldflags $LDFLAGS" github.com/foxcpp/maddy/cmd/{maddy,maddyctl}@$MADDYVERSION
+}
+
+install_executables() {
+    echo 'Installing maddy...' >&2
+
+    $SUDO mkdir -p "$PREFIX/bin"
+    $SUDO cp --remove-destination "$GOPATH/bin/maddy" "$GOPATH/bin/maddyctl" "$PREFIX/bin/"
+}
+
+install_systemd() {
+    echo 'Downloading and installing systemd unit files...' >&2
+
+    wget -q "https://raw.githubusercontent.com/foxcpp/maddy/$MADDYVERSION/dist/systemd/maddy.service" -O maddy.service
+    wget -q "https://raw.githubusercontent.com/foxcpp/maddy/$MADDYVERSION/dist/systemd/maddy@.service" -O maddy@.service
+
+    sed -Ei "s!/usr/bin!$PREFIX/bin!g" maddy.service maddy@.service
+
+    $SUDO mkdir -p "$SYSTEMDUNITS/system/"
+    $SUDO cp maddy.service maddy@.service "$SYSTEMDUNITS/system/"
+    $SUDO systemctl daemon-reload
+}
+
+create_user() {
+    echo 'Creating maddy user and group...' >&2
+
+    $SUDO useradd -UMr -s /sbin/nologin maddy || true
+}
+
+install_config() {
+    echo 'Using configuration path:' $CONFPATH
+    if ! [ -e "$CONFPATH" ]; then
+        echo 'Downloading and installing default configuration...' >&2
+
+        wget -q "https://raw.githubusercontent.com/foxcpp/maddy/$MADDYVERSION/maddy.conf" -O maddy.conf
+        $SUDO mkdir -p /etc/maddy/
+
+        host=`hostname`
+        read -p "What's your domain, btw? [$host] > " DOMAIN
+        if [ "$DOMAIN" = "" ]; then
+            DOMAIN=$host
+        fi
+        echo 'Good, I will put that into configuration for you.' >&2
+
+        sed -Ei "s/^\\$\\(primary_domain\) = .+$/$\(primary_domain\) = $DOMAIN/" maddy.conf
+        sed -Ei "s/^\\$\\(hostname\) = .+$/$\(hostname\) = $DOMAIN/" maddy.conf
+
+        $SUDO cp maddy.conf $CONFPATH
+    else
+        echo "Configuration already exists in /etc/maddy/maddy.conf, skipping defaults installation." >&2
+    fi
+}
+
+run() {
+    mkdir -p maddy-setup/
+    cd maddy-setup/
+
+    ensure_go_toolchain
+    download_and_compile
+    install_executables
+    install_systemd
+    install_config
+    create_user
+
+    echo "Okay, almost ready." >&2
+    echo "It's up to you to figure out TLS certificates and DNS stuff, though." >&2
+    echo "Here is the tutorial to help you:" >&2
+    echo "https://github.com/foxcpp/maddy/wiki/Tutorial:-Setting-up-a-mail-server-with-maddy" >&2
+}
+
+run
