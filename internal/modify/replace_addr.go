aa1804c66daeddb9e5a7076b7d2771c71ccf7876
fox.cpp <fox.cpp@disroot.org>
2020-03-06 04:19:28 +0300

    table: Merge 'replace_sender', 'replace_rcpt' into 'alias'
    With 'regexp' and 'static' tables, separate implementations in replace_*
    are not necessary.

--- internal/modify/replace_addr.go
+++ internal/modify/replace_addr.go
@@ -3,116 +3,45 @@ package modify
 import (
 	"context"
 	"fmt"
-	"regexp"
 	"strings"
 
 	"github.com/emersion/go-message/textproto"
 	"github.com/foxcpp/maddy/internal/address"
 	"github.com/foxcpp/maddy/internal/buffer"
 	"github.com/foxcpp/maddy/internal/config"
+	modconfig "github.com/foxcpp/maddy/internal/config/module"
 	"github.com/foxcpp/maddy/internal/module"
 )
 
 // replaceAddr is a simple module that replaces matching sender (or recipient) address
-// in messages.
+// in messages using module.Table implementation.
 //
 // If created with modName = "replace_sender", it will change sender address.
 // If created with modName = "replace_rcpt", it will change recipient addresses.
-//
-// Matching is done by either regexp or plain string. Module arguments are as
-// arguments for brevity..
 type replaceAddr struct {
-	modName  string
-	instName string
+	modName    string
+	instName   string
+	inlineArgs []string
 
-	inlineFromArg string
-	inlineToArg   string
 	replaceSender bool
 	replaceRcpt   bool
-
-	// Matchers. Only one is used.
-	fromString string
-	fromRegex  *regexp.Regexp
-
-	// Replacement string
-	to string
+	table         module.Table
 }
 
 func NewReplaceAddr(modName, instName string, _, inlineArgs []string) (module.Module, error) {
 	r := replaceAddr{
 		modName:       modName,
 		instName:      instName,
+		inlineArgs:    inlineArgs,
 		replaceSender: modName == "replace_sender",
 		replaceRcpt:   modName == "replace_rcpt",
-	}
-
-	switch len(inlineArgs) {
-	case 0:
-		// Not inline definition.
-	case 2:
-		r.inlineFromArg = inlineArgs[0]
-		r.inlineToArg = inlineArgs[1]
-	default:
-		return nil, fmt.Errorf("%s: invalid amount of inline arguments", modName)
 	}
 
 	return &r, nil
 }
 
-func (r *replaceAddr) Init(m *config.Map) error {
-	var fromCfg, toCfg string
-	m.String("from", false, false, r.inlineFromArg, &fromCfg)
-	m.String("to", false, false, r.inlineToArg, &toCfg)
-	if _, err := m.Process(); err != nil {
-		return err
-	}
-
-	if fromCfg == "" {
-		return fmt.Errorf("%s: missing 'from' argument or directive", r.modName)
-	}
-	if toCfg == "" {
-		return fmt.Errorf("%s: missing 'to' argument or directive", r.modName)
-	}
-
-	if strings.HasPrefix(fromCfg, "/") {
-		if !strings.HasSuffix(fromCfg, "/") {
-			return fmt.Errorf("%s: missing trailing slash in 'from' value", r.modName)
-		}
-
-		regex := fromCfg[1 : len(fromCfg)-1]
-
-		// Regexp should match entire string, so add anchors
-		// if they are not present.
-		if !strings.HasPrefix(regex, "^") {
-			regex = "^" + regex
-		}
-		if !strings.HasSuffix(regex, "$") {
-			regex = regex + "$"
-		}
-
-		regex = "(?i)" + regex
-
-		var err error
-		r.fromRegex, err = regexp.Compile(regex)
-		if err != nil {
-			return fmt.Errorf("%s: %v", r.modName, err)
-		}
-	}
-	if strings.HasSuffix(fromCfg, "/") && !strings.HasPrefix(fromCfg, "/") {
-		return fmt.Errorf("%s: missing leading slash in 'from' value", r.modName)
-	}
-
-	r.fromString = fromCfg
-
-	if strings.HasPrefix(toCfg, "/") || strings.HasSuffix(toCfg, "/") {
-		return fmt.Errorf("%s: can't use regexp in 'to' value", r.modName)
-	}
-	if r.fromRegex == nil && strings.Contains(toCfg, "$") {
-		return fmt.Errorf("%s: can't reference capture groups in 'to' if 'from' is not a regexp", r.modName)
-	}
-	r.to = toCfg
-
-	return nil
+func (r *replaceAddr) Init(cfg *config.Map) error {
+	return modconfig.ModuleFromNode(r.inlineArgs, cfg.Block, cfg.Globals, &r.table)
 }
 
 func (r replaceAddr) Name() string {
@@ -150,25 +79,46 @@ func (r replaceAddr) Close() error {
 }
 
 func (r replaceAddr) rewrite(val string) (string, error) {
-	if r.fromRegex == nil {
-		if address.Equal(r.fromString, val) {
-			return r.to, nil
-		}
-		return val, nil
+	normAddr, err := address.ForLookup(val)
+	if err != nil {
+		return val, fmt.Errorf("malformed address: %v", err)
 	}
 
-	normVal, err := address.ForLookup(val)
+	replacement, ok, err := r.table.Lookup(normAddr)
 	if err != nil {
-		// Ouch. Should not happen at this point.
 		return val, err
 	}
+	if ok {
+		if !address.Valid(replacement) {
+			return "", fmt.Errorf("refusing to replace recipient with the invalid address %s", replacement)
+		}
+		return replacement, nil
+	}
 
-	indx := r.fromRegex.FindStringSubmatchIndex(normVal)
-	if indx == nil {
+	mbox, domain, err := address.Split(normAddr)
+	if err != nil {
+		// If we have malformed address here, something is really wrong, but let's
+		// ignore it silently then anyway.
 		return val, nil
 	}
 
-	return string(r.fromRegex.ExpandString([]byte{}, r.to, val, indx)), nil
+	// mbox is already normalized, since it is a part of address.ForLookup
+	// result.
+	replacement, ok, err = r.table.Lookup(mbox)
+	if err != nil {
+		return val, err
+	}
+	if ok {
+		if strings.Contains(replacement, "@") && !strings.HasPrefix(replacement, `"`) && !strings.HasSuffix(replacement, `"`) {
+			if !address.Valid(replacement) {
+				return "", fmt.Errorf("refusing to replace recipient with invalid address %s", replacement)
+			}
+			return replacement, nil
+		}
+		return replacement + "@" + domain, nil
+	}
+
+	return val, nil
 }
 
 func init() {
--- internal/modify/replace_addr_test.go
+++ internal/modify/replace_addr_test.go
@@ -5,45 +5,75 @@ import (
 	"testing"
 
 	"github.com/foxcpp/maddy/internal/config"
+	"github.com/foxcpp/maddy/internal/testutils"
 )
 
-func replaceAddrFromArgs(t *testing.T, modName, from, to string) *replaceAddr {
-	r, err := NewReplaceAddr(modName, "", nil, []string{from, to})
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := r.Init(&config.Map{Block: config.Node{}}); err != nil {
-		t.Fatal(err)
-	}
-
-	return r.(*replaceAddr)
-}
-
 func testReplaceAddr(t *testing.T, modName string, rewriter func(*replaceAddr, context.Context, string) (string, error)) {
-	test := func(from, to string, input, expectedOutput string) {
+	test := func(addr, expected string, aliases map[string]string) {
 		t.Helper()
 
-		r := replaceAddrFromArgs(t, modName, from, to)
-		output, err := rewriter(r, context.Background(), input)
+		mod, err := NewReplaceAddr(modName, "", nil, []string{"dummy"})
 		if err != nil {
 			t.Fatal(err)
 		}
-		if output != expectedOutput {
-			t.Fatalf("wrong result: %s != %s", output, expectedOutput)
+		m := mod.(*replaceAddr)
+		if err := m.Init(config.NewMap(nil, config.Node{})); err != nil {
+			t.Fatal(err)
+		}
+		m.table = testutils.Table{M: aliases}
+
+		var actual string
+		if modName == "replace_sender" {
+			actual, err = m.RewriteSender(context.Background(), addr)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+		if modName == "replace_rcpt" {
+			actual, err = m.RewriteRcpt(context.Background(), addr)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		if actual != expected {
+			t.Errorf("want %s, got %s", expected, actual)
 		}
 	}
 
-	test("test@example.org", "test2@example.org", "test@example.org", "test2@example.org")
-	test("test@EXAmple.org", "test2@example.org", "teST@exaMPLe.org", "test2@example.org")
-	test(`/test@example\.org/`, "test2@example.org", "test@example.org", "test2@example.org")
-	test(`/test@EXAmple\.org/`, "test2@example.org", "teST@exaMPLe.org", "test2@example.org")
-	test(`/example/`, "test2@example.org", "teST@exaMPLe.org", "teST@exaMPLe.org")
-	test(`/(.+)@example\.org/`, "$1@example.com", "test@example.org", "test@example.com")
-	test(`/(.+)@example\.org/`, "$1@example.com", "teST@example.org", "teST@example.com")
-	test(`/(.+)@example\.org/`, "$1@example.com", "teST@example.org", "teST@example.com")
-	test("rcpt@\u00E9.example.com", "rcpt@foo.example.com", "rcpt@E\u0301.example.com", "rcpt@foo.example.com")
-	test(`/rcpt@é\.example\.com/`, "rcpt@foo.example.com", "rcpt@E\u0301.example.com", "rcpt@foo.example.com")
-	test("rcpt@E\u0301.example.com", "rcpt@foo.example.com", "rcpt@\u00E9.example.com", "rcpt@foo.example.com")
+	test("test@example.org", "test@example.org", nil)
+	test("postmaster", "postmaster", nil)
+	test("test@example.com", "test@example.org",
+		map[string]string{"test@example.com": "test@example.org"})
+	test(`"\"test @ test\""@example.com`, "test@example.org",
+		map[string]string{`"\"test @ test\""@example.com`: "test@example.org"})
+	test(`test@example.com`, `"\"test @ test\""@example.org`,
+		map[string]string{`test@example.com`: `"\"test @ test\""@example.org`})
+	test(`"\"test @ test\""@example.com`, `"\"b @ b\""@example.com`,
+		map[string]string{`"\"test @ test\""`: `"\"b @ b\""`})
+	test("TeSt@eXAMple.com", "test@example.org",
+		map[string]string{"test@example.com": "test@example.org"})
+	test("test@example.com", "test2@example.com",
+		map[string]string{"test": "test2"})
+	test("test@example.com", "test2@example.org",
+		map[string]string{"test": "test2@example.org"})
+	test("postmaster", "test2@example.org",
+		map[string]string{"postmaster": "test2@example.org"})
+	test("TeSt@examPLE.com", "test2@example.com",
+		map[string]string{"test": "test2"})
+	test("test@example.com", "test3@example.com",
+		map[string]string{
+			"test@example.com": "test3@example.com",
+			"test":             "test2",
+		})
+	test("rcpt@E\u0301.example.com", "rcpt@foo.example.com",
+		map[string]string{
+			"rcpt@\u00E9.example.com": "rcpt@foo.example.com",
+		})
+	test("E\u0301@foo.example.com", "rcpt@foo.example.com",
+		map[string]string{
+			"\u00E9@foo.example.com": "rcpt@foo.example.com",
+		})
 }
 
 func TestReplaceAddr_RewriteSender(t *testing.T) {
