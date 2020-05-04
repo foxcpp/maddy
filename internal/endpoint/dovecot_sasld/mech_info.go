a1f49f3ddc5fd0d6acf474075ccb061c28a4cf3f
fox.cpp <fox.cpp@disroot.org>
2020-05-05 01:06:25 +0300

    endpoint/dovecode_sasld: Fill mechInfo table

--- internal/endpoint/dovecot_sasld/mech_info.go
+++ internal/endpoint/dovecot_sasld/mech_info.go
@@ -1,5 +1,15 @@
 package dovecotsasld
 
-import dovecotsasl "github.com/foxcpp/go-dovecot-sasl"
+import (
+	"github.com/emersion/go-sasl"
+	dovecotsasl "github.com/foxcpp/go-dovecot-sasl"
+)
 
-var mechInfo = map[string]dovecotsasl.Mechanism{}
+var mechInfo = map[string]dovecotsasl.Mechanism{
+	sasl.Plain: {
+		Plaintext: true,
+	},
+	sasl.Login: {
+		Plaintext: true,
+	},
+}
