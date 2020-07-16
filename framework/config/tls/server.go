11e5b2854629c178732414f54d432572f920ffe2
fox.cpp <fox.cpp@disroot.org>
2020-07-17 00:34:56 +0300

    config/tls: Remove unused code
    GODEBUG line is no longer necessary since TLS 1.3 is enabled by default
    since Go 1.13 and we now require Go 1.14.

--- framework/config/tls/server.go
+++ framework/config/tls/server.go
@@ -1,17 +1,9 @@
 package tls
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
 	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"math/big"
-	"net"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/foxcpp/maddy/framework/config"
 	modconfig "github.com/foxcpp/maddy/framework/config/module"
@@ -120,47 +112,4 @@ func readTLSBlock(globals map[string]interface{}, blockNode config.Node) (*TLSCo
 		loader:  loader,
 		baseCfg: &baseCfg,
 	}, nil
-}
-
-func makeSelfSignedCert(config *tls.Config) error {
-	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		return err
-	}
-
-	notBefore := time.Now()
-	notAfter := notBefore.Add(24 * time.Hour * 7)
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		return err
-	}
-	cert := &x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject:      pkix.Name{Organization: []string{"Maddy Self-Signed"}},
-		NotBefore:    notBefore,
-		NotAfter:     notAfter,
-		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-	}
-	if ip := net.ParseIP(config.ServerName); ip != nil {
-		cert.IPAddresses = append(cert.IPAddresses, ip)
-	} else {
-		cert.DNSNames = append(cert.DNSNames, config.ServerName)
-	}
-	derBytes, err := x509.CreateCertificate(rand.Reader, cert, cert, &privKey.PublicKey, privKey)
-	if err != nil {
-		return err
-	}
-
-	config.Certificates = append(config.Certificates, tls.Certificate{
-		Certificate: [][]byte{derBytes},
-		PrivateKey:  privKey,
-		Leaf:        cert,
-	})
-	return nil
-}
-
-func init() {
-	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
 }
