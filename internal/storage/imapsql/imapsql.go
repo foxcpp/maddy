a99e6f7c5b1d4491512bedf68e8fb40d34ad12ef
fox.cpp <fox.cpp@disroot.org>
2020-09-13 21:48:57 +0300

    storage/imapsql: Move delivery logic to a separate file

--- internal/storage/imapsql/imapsql.go
+++ internal/storage/imapsql/imapsql.go
@@ -26,7 +26,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package imapsql
 
 import (
-	"context"
 	"crypto/sha1"
 	"encoding/hex"
 	"errors"
@@ -34,25 +33,19 @@ import (
 	"os"
 	"path/filepath"
 	"runtime/debug"
-	"runtime/trace"
 	"strconv"
 	"strings"
 
 	"github.com/emersion/go-imap"
 	sortthread "github.com/emersion/go-imap-sortthread"
-	specialuse "github.com/emersion/go-imap-specialuse"
 	"github.com/emersion/go-imap/backend"
-	"github.com/emersion/go-message/textproto"
 	imapsql "github.com/foxcpp/go-imap-sql"
 	"github.com/foxcpp/maddy/framework/address"
-	"github.com/foxcpp/maddy/framework/buffer"
 	"github.com/foxcpp/maddy/framework/config"
 	modconfig "github.com/foxcpp/maddy/framework/config/module"
 	"github.com/foxcpp/maddy/framework/dns"
-	"github.com/foxcpp/maddy/framework/exterrors"
 	"github.com/foxcpp/maddy/framework/log"
 	"github.com/foxcpp/maddy/framework/module"
-	"github.com/foxcpp/maddy/internal/target"
 	"github.com/foxcpp/maddy/internal/updatepipe"
 	"golang.org/x/text/secure/precis"
 
@@ -79,140 +72,8 @@ type Storage struct {
 	filters module.IMAPFilter
 }
 
-type delivery struct {
-	store    *Storage
-	msgMeta  *module.MsgMetadata
-	d        imapsql.Delivery
-	mailFrom string
-
-	addedRcpts map[string]struct{}
-}
-
-func (d *delivery) String() string {
-	return d.store.Name() + ":" + d.store.InstanceName()
-}
-
-func (d *delivery) AddRcpt(ctx context.Context, rcptTo string) error {
-	defer trace.StartRegion(ctx, "sql/AddRcpt").End()
-
-	accountName, err := prepareUsername(rcptTo)
-	if err != nil {
-		return &exterrors.SMTPError{
-			Code:         501,
-			EnhancedCode: exterrors.EnhancedCode{5, 1, 1},
-			Message:      "User does not exist",
-			TargetName:   "imapsql",
-			Err:          err,
-		}
-	}
-
-	accountName = strings.ToLower(accountName)
-	if _, ok := d.addedRcpts[accountName]; ok {
-		return nil
-	}
-
-	// This header is added to the message only for that recipient.
-	// go-imap-sql does certain optimizations to store the message
-	// with small amount of per-recipient data in a efficient way.
-	userHeader := textproto.Header{}
-	userHeader.Add("Delivered-To", accountName)
-
-	if err := d.d.AddRcpt(accountName, userHeader); err != nil {
-		if err == imapsql.ErrUserDoesntExists || err == backend.ErrNoSuchMailbox {
-			return &exterrors.SMTPError{
-				Code:         550,
-				EnhancedCode: exterrors.EnhancedCode{5, 1, 1},
-				Message:      "User does not exist",
-				TargetName:   "imapsql",
-				Err:          err,
-			}
-		}
-		if _, ok := err.(imapsql.SerializationError); ok {
-			return &exterrors.SMTPError{
-				Code:         453,
-				EnhancedCode: exterrors.EnhancedCode{4, 3, 2},
-				Message:      "Storage access serialiation problem, try again later",
-				TargetName:   "imapsql",
-				Err:          err,
-			}
-		}
-		return err
-	}
-
-	d.addedRcpts[accountName] = struct{}{}
-	return nil
-}
-
-func (d *delivery) Body(ctx context.Context, header textproto.Header, body buffer.Buffer) error {
-	defer trace.StartRegion(ctx, "sql/Body").End()
-
-	if !d.msgMeta.Quarantine && d.store.filters != nil {
-		for rcpt := range d.addedRcpts {
-			folder, flags, err := d.store.filters.IMAPFilter(rcpt, d.msgMeta, header, body)
-			if err != nil {
-				d.store.Log.Error("IMAPFilter failed", err, "rcpt", rcpt)
-				continue
-			}
-			d.d.UserMailbox(rcpt, folder, flags)
-		}
-	}
-
-	if d.msgMeta.Quarantine {
-		if err := d.d.SpecialMailbox(specialuse.Junk, d.store.junkMbox); err != nil {
-			if _, ok := err.(imapsql.SerializationError); ok {
-				return &exterrors.SMTPError{
-					Code:         453,
-					EnhancedCode: exterrors.EnhancedCode{4, 3, 2},
-					Message:      "Storage access serialiation problem, try again later",
-					TargetName:   "imapsql",
-					Err:          err,
-				}
-			}
-			return err
-		}
-	}
-
-	header = header.Copy()
-	header.Add("Return-Path", "<"+target.SanitizeForHeader(d.mailFrom)+">")
-	err := d.d.BodyParsed(header, body.Len(), body)
-	if _, ok := err.(imapsql.SerializationError); ok {
-		return &exterrors.SMTPError{
-			Code:         453,
-			EnhancedCode: exterrors.EnhancedCode{4, 3, 2},
-			Message:      "Storage access serialiation problem, try again later",
-			TargetName:   "imapsql",
-			Err:          err,
-		}
-	}
-	return err
-}
-
-func (d *delivery) Abort(ctx context.Context) error {
-	defer trace.StartRegion(ctx, "sql/Abort").End()
-
-	return d.d.Abort()
-}
-
-func (d *delivery) Commit(ctx context.Context) error {
-	defer trace.StartRegion(ctx, "sql/Commit").End()
-
-	return d.d.Commit()
-}
-
-func (store *Storage) Start(ctx context.Context, msgMeta *module.MsgMetadata, mailFrom string) (module.Delivery, error) {
-	defer trace.StartRegion(ctx, "sql/Start").End()
-
-	return &delivery{
-		store:      store,
-		msgMeta:    msgMeta,
-		mailFrom:   mailFrom,
-		d:          store.Back.NewDelivery(),
-		addedRcpts: map[string]struct{}{},
-	}, nil
-}
-
 func (store *Storage) Name() string {
-	return "sql"
+	return "imapsql"
 }
 
 func (store *Storage) InstanceName() string {
