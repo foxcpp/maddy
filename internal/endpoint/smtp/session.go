foxcpp/maddy

commit 1c42a705334b3d34b4b3ddff74c654773a4c0ef5

endpoint/smtp: Fix handling of empty messages in auto-buffer code
Add the missing os.MkdirAll for "fs" buffer mode along the way.
And add integration tests for all that.

internal/endpoint/smtp/smtp.go
@@ -149,16 +149,20 @@ func autoBufferMode(maxSize int, dir string) func(io.Reader) (buffer.Buffer, err
 	return func(r io.Reader) (buffer.Buffer, error) {
 		// First try to read up to N bytes.
 		initial := make([]byte, maxSize)
-		actualSize, err := io.ReadFull(r, initial)
+		actualSize, err := r.Read(initial)
 		if err != nil {
-			if err == io.ErrUnexpectedEOF {
-				// Ok, the message is smaller than N. Make a MemoryBuffer and
-				// handle it in RAM.
+			if err == io.EOF {
 				log.Debugln("autobuffer: keeping the message in RAM")
 				return buffer.MemoryBuffer{Slice: initial[:actualSize]}, nil
 			}
 			// Some I/O error happened, bail out.
 			return nil, err
+		}
+		if actualSize < maxSize {
+			// Ok, the message is smaller than N. Make a MemoryBuffer and
+			// handle it in RAM.
+			log.Debugln("autobuffer: keeping the message in RAM")
+			return buffer.MemoryBuffer{Slice: initial[:actualSize]}, nil
 		}
 
 		log.Debugln("autobuffer: spilling the message to the FS")
@@ -181,6 +185,9 @@ func bufferModeDirective(m *config.Map, node config.Node) (interface{}, error) {
 		return buffer.BufferInMemory, nil
 	case "fs":
 		path := filepath.Join(config.StateDirectory, "buffer")
+		if err := os.MkdirAll(path, 0700); err != nil {
+			return nil, err
+		}
 		switch len(node.Args) {
 		case 2:
 			path = node.Args[1]

internal/endpoint/smtp/smtp_test.go
@@ -418,6 +418,45 @@ func TestSMTPDelivery_AbortData(t *testing.T) {
 	}
 }
 
+func TestSMTPDelivery_EmptyMessage(t *testing.T) {
+	tgt := testutils.Target{}
+	endp := testEndpoint(t, "smtp", nil, &tgt, nil, nil)
+	defer endp.Close()
+
+	cl, err := smtp.Dial("127.0.0.1:" + testPort)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cl.Close()
+
+	if err := cl.Hello("mx.example.org"); err != nil {
+		t.Fatal(err)
+	}
+	if err := cl.Mail("sender@example.org", nil); err != nil {
+		t.Fatal(err)
+	}
+	if err := cl.Rcpt("test@example.com"); err != nil {
+		t.Fatal(err)
+	}
+	data, err := cl.Data()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := data.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(250 * time.Millisecond)
+
+	if len(tgt.Messages) != 1 {
+		t.Fatal("Expected 1 message, got", len(tgt.Messages))
+	}
+	msg := tgt.Messages[0]
+	if len(msg.Body) != 0 {
+		t.Fatal("Expected an empty body, got", len(msg.Body))
+	}
+}
+
 func TestSMTPDelivery_AbortLogout(t *testing.T) {
 	tgt := testutils.Target{}
 	endp := testEndpoint(t, "smtp", nil, &tgt, nil, nil)

tests/smtp_autobuffer_test.go
@@ -1,0 +1,254 @@
+//+build integration,cgo,!nosqlite3
+
+/*
+Maddy Mail Server - Composable all-in-one email server.
+Copyright © 2019-2020 Max Mazurov <fox.cpp@disroot.org>, Maddy Mail Server contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package tests_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/foxcpp/maddy/tests"
+)
+
+func TestSMTPEndpoint_FileBuffer(tt *testing.T) {
+	run := func(tt *testing.T, bufferOpt string) {
+		tt.Parallel()
+		t := tests.NewT(tt)
+
+		t.DNS(nil)
+		t.Port("imap")
+		t.Port("smtp")
+		t.Config(`
+		storage.imapsql test_store {
+			driver sqlite3
+			dsn imapsql.db
+		}
+
+		imap tcp://127.0.0.1:{env:TEST_PORT_imap} {
+			tls off
+
+			auth dummy
+			storage &test_store
+		}
+
+		smtp tcp://127.0.0.1:{env:TEST_PORT_smtp} {
+			hostname maddy.test
+			tls off
+			buffer ` + bufferOpt + `
+
+			deliver_to &test_store
+		}
+	`)
+		t.Run(2)
+		defer t.Close()
+
+		imapConn := t.Conn("imap")
+		defer imapConn.Close()
+		imapConn.ExpectPattern(`\* OK *`)
+		imapConn.Writeln(". LOGIN testusr@maddy.test 1234")
+		imapConn.ExpectPattern(". OK *")
+		imapConn.Writeln(". SELECT INBOX")
+		imapConn.ExpectPattern(`\* *`)
+		imapConn.ExpectPattern(`\* *`)
+		imapConn.ExpectPattern(`\* *`)
+		imapConn.ExpectPattern(`\* *`)
+		imapConn.ExpectPattern(`\* *`)
+		imapConn.ExpectPattern(`\* *`)
+		imapConn.ExpectPattern(`. OK *`)
+
+		smtpConn := t.Conn("smtp")
+		defer smtpConn.Close()
+		smtpConn.SMTPNegotation("localhost", nil, nil)
+		smtpConn.Writeln("MAIL FROM:<sender@maddy.test>")
+		smtpConn.ExpectPattern("2*")
+		smtpConn.Writeln("RCPT TO:<testusr@maddy.test>")
+		smtpConn.ExpectPattern("2*")
+		smtpConn.Writeln("DATA")
+		smtpConn.ExpectPattern("354 *")
+		smtpConn.Writeln("From: <sender@maddy.test>")
+		smtpConn.Writeln("To: <testusr@maddy.test>")
+		smtpConn.Writeln("Subject: Hi!")
+		smtpConn.Writeln("")
+		smtpConn.Writeln("AAAAABBBBBB")
+		smtpConn.Writeln(".")
+		smtpConn.ExpectPattern("2*")
+
+		time.Sleep(500 * time.Millisecond)
+
+		imapConn.Writeln(". NOOP")
+		imapConn.ExpectPattern(`\* 1 EXISTS`)
+		imapConn.ExpectPattern(". OK *")
+
+		imapConn.Writeln(". FETCH 1 (BODY.PEEK[])")
+		imapConn.ExpectPattern(`\* 1 FETCH (BODY\[\] {312}*`)
+		imapConn.Expect(`Delivered-To: testusr@maddy.test`)
+		imapConn.Expect(`Return-Path: <sender@maddy.test>`)
+		imapConn.Expect(`Received: from localhost (localhost [127.0.0.1]) by maddy.test`)
+		imapConn.ExpectPattern(` (envelope-sender <sender@maddy.test>) with ESMTP id *; *`)
+		imapConn.ExpectPattern(` *`)
+		imapConn.Expect("From: <sender@maddy.test>")
+		imapConn.Expect("To: <testusr@maddy.test>")
+		imapConn.Expect("Subject: Hi!")
+		imapConn.Expect("")
+		imapConn.Expect("AAAAABBBBBB")
+		imapConn.Expect(")")
+		imapConn.ExpectPattern(`. OK *`)
+	}
+
+	tt.Run("ram", func(tt *testing.T) { run(tt, "ram") })
+	tt.Run("fs", func(tt *testing.T) { run(tt, "fs") })
+}
+
+func TestSMTPEndpoint_Autobuffer(tt *testing.T) {
+	tt.Parallel()
+	t := tests.NewT(tt)
+
+	t.DNS(nil)
+	t.Port("imap")
+	t.Port("smtp")
+	t.Config(`
+		storage.imapsql test_store {
+			driver sqlite3
+			dsn imapsql.db
+		}
+
+		imap tcp://127.0.0.1:{env:TEST_PORT_imap} {
+			tls off
+
+			auth dummy
+			storage &test_store
+		}
+
+		smtp tcp://127.0.0.1:{env:TEST_PORT_smtp} {
+			hostname maddy.test
+			tls off
+			buffer auto 5b
+
+			deliver_to &test_store
+		}
+	`)
+	t.Run(2)
+	defer t.Close()
+
+	imapConn := t.Conn("imap")
+	defer imapConn.Close()
+	imapConn.ExpectPattern(`\* OK *`)
+	imapConn.Writeln(". LOGIN testusr@maddy.test 1234")
+	imapConn.ExpectPattern(". OK *")
+	imapConn.Writeln(". SELECT INBOX")
+	imapConn.ExpectPattern(`\* *`)
+	imapConn.ExpectPattern(`\* *`)
+	imapConn.ExpectPattern(`\* *`)
+	imapConn.ExpectPattern(`\* *`)
+	imapConn.ExpectPattern(`\* *`)
+	imapConn.ExpectPattern(`\* *`)
+	imapConn.ExpectPattern(`. OK *`)
+
+	smtpConn := t.Conn("smtp")
+	defer smtpConn.Close()
+	smtpConn.SMTPNegotation("localhost", nil, nil)
+	smtpConn.Writeln("MAIL FROM:<sender@maddy.test>")
+	smtpConn.ExpectPattern("2*")
+	smtpConn.Writeln("RCPT TO:<testusr@maddy.test>")
+	smtpConn.ExpectPattern("2*")
+	smtpConn.Writeln("DATA")
+	smtpConn.ExpectPattern("354 *")
+	smtpConn.Writeln("From: <sender@maddy.test>")
+	smtpConn.Writeln("To: <testusr@maddy.test>")
+	smtpConn.Writeln("Subject: Hi!")
+	smtpConn.Writeln("")
+	smtpConn.Writeln("AAAAABBBBBB")
+	smtpConn.Writeln(".")
+	smtpConn.ExpectPattern("2*")
+
+	smtpConn.Writeln("MAIL FROM:<sender@maddy.test>")
+	smtpConn.ExpectPattern("2*")
+	smtpConn.Writeln("RCPT TO:<testusr@maddy.test>")
+	smtpConn.ExpectPattern("2*")
+	smtpConn.Writeln("DATA")
+	smtpConn.ExpectPattern("354 *")
+	smtpConn.Writeln("From: <sender@maddy.test>")
+	smtpConn.Writeln("To: <testusr@maddy.test>")
+	smtpConn.Writeln("Subject: Hi!")
+	smtpConn.Writeln("")
+	smtpConn.Writeln(".")
+	smtpConn.ExpectPattern("2*")
+
+	smtpConn.Writeln("MAIL FROM:<sender@maddy.test>")
+	smtpConn.ExpectPattern("2*")
+	smtpConn.Writeln("RCPT TO:<testusr@maddy.test>")
+	smtpConn.ExpectPattern("2*")
+	smtpConn.Writeln("DATA")
+	smtpConn.ExpectPattern("354 *")
+	smtpConn.Writeln("From: <sender@maddy.test>")
+	smtpConn.Writeln("To: <testusr@maddy.test>")
+	smtpConn.Writeln("Subject: Hi!")
+	smtpConn.Writeln("")
+	smtpConn.Writeln("AAA")
+	smtpConn.Writeln(".")
+	smtpConn.ExpectPattern("2*")
+
+	time.Sleep(500 * time.Millisecond)
+
+	imapConn.Writeln(". NOOP")
+	// This will break with go-imap v2 upgrade merging updates.
+	imapConn.ExpectPattern(`\* 1 EXISTS`)
+	imapConn.ExpectPattern(`\* 2 EXISTS`)
+	imapConn.ExpectPattern(`\* 3 EXISTS`)
+	imapConn.ExpectPattern(". OK *")
+
+	imapConn.Writeln(". FETCH 1:3 (BODY.PEEK[])")
+	imapConn.ExpectPattern(`\* 1 FETCH (BODY\[\] {312}*`)
+	imapConn.Expect(`Delivered-To: testusr@maddy.test`)
+	imapConn.Expect(`Return-Path: <sender@maddy.test>`)
+	imapConn.Expect(`Received: from localhost (localhost [127.0.0.1]) by maddy.test`)
+	imapConn.ExpectPattern(` (envelope-sender <sender@maddy.test>) with ESMTP id *; *`)
+	imapConn.ExpectPattern(` *`)
+	imapConn.Expect("From: <sender@maddy.test>")
+	imapConn.Expect("To: <testusr@maddy.test>")
+	imapConn.Expect("Subject: Hi!")
+	imapConn.Expect("")
+	imapConn.Expect("AAAAABBBBBB")
+	imapConn.Expect(")")
+	imapConn.ExpectPattern(`\* 2 FETCH (BODY\[\] {299}*`)
+	imapConn.Expect(`Delivered-To: testusr@maddy.test`)
+	imapConn.Expect(`Return-Path: <sender@maddy.test>`)
+	imapConn.Expect(`Received: from localhost (localhost [127.0.0.1]) by maddy.test`)
+	imapConn.ExpectPattern(` (envelope-sender <sender@maddy.test>) with ESMTP id *; *`)
+	imapConn.ExpectPattern(` *`)
+	imapConn.Expect("From: <sender@maddy.test>")
+	imapConn.Expect("To: <testusr@maddy.test>")
+	imapConn.Expect("Subject: Hi!")
+	imapConn.Expect("")
+	imapConn.Expect(")")
+	imapConn.ExpectPattern(`\* 3 FETCH (BODY\[\] {304}*`)
+	imapConn.Expect(`Delivered-To: testusr@maddy.test`)
+	imapConn.Expect(`Return-Path: <sender@maddy.test>`)
+	imapConn.Expect(`Received: from localhost (localhost [127.0.0.1]) by maddy.test`)
+	imapConn.ExpectPattern(` (envelope-sender <sender@maddy.test>) with ESMTP id *; *`)
+	imapConn.ExpectPattern(` *`)
+	imapConn.Expect("From: <sender@maddy.test>")
+	imapConn.Expect("To: <testusr@maddy.test>")
+	imapConn.Expect("Subject: Hi!")
+	imapConn.Expect("")
+	imapConn.Expect("AAA")
+	imapConn.Expect(")")
+	imapConn.ExpectPattern(`. OK *`)
+}

commit 925b758e094335807f1928b59d1ceffac82b8a7a

endpoint/smtp: Actually fix autobuffer function
Regressed in 1c42a705334b3d34b4b3ddff74c654773a4c0ef5.
It truncates messages to I/O buffer size (4096 bytes).
If first 4096 bytes cover head then it causes "unexpected EOF" (see
 #300), otherwise it silently breaks the message.

internal/endpoint/smtp/session.go
@@ -336,7 +336,7 @@ func (s *Session) prepareBody(ctx context.Context, r io.Reader) (textproto.Heade
 	bufr := bufio.NewReader(r)
 	header, err := textproto.ReadHeader(bufr)
 	if err != nil {
-		return textproto.Header{}, nil, err
+		return textproto.Header{}, nil, fmt.Errorf("I/O error while parsing header: %w", err)
 	}
 
 	if s.endp.submission {
@@ -348,7 +348,7 @@ func (s *Session) prepareBody(ctx context.Context, r io.Reader) (textproto.Heade
 
 	buf, err := s.endp.buffer(bufr)
 	if err != nil {
-		return textproto.Header{}, nil, err
+		return textproto.Header{}, nil, fmt.Errorf("I/O error while writing buffer: %w", err)
 	}
 
 	return header, buf, nil

internal/endpoint/smtp/smtp.go
@@ -149,11 +149,15 @@ func autoBufferMode(maxSize int, dir string) func(io.Reader) (buffer.Buffer, err
 	return func(r io.Reader) (buffer.Buffer, error) {
 		// First try to read up to N bytes.
 		initial := make([]byte, maxSize)
-		actualSize, err := r.Read(initial)
+		actualSize, err := io.ReadFull(r, initial)
 		if err != nil {
-			if err == io.EOF {
-				log.Debugln("autobuffer: keeping the message in RAM")
+			if err == io.ErrUnexpectedEOF {
+				log.Debugln("autobuffer: keeping the message in RAM (read", actualSize, "bytes, got EOF)")
 				return buffer.MemoryBuffer{Slice: initial[:actualSize]}, nil
+			}
+			if err == io.EOF {
+				// Special case: message with empty body.
+				return buffer.MemoryBuffer{}, nil
 			}
 			// Some I/O error happened, bail out.
 			return nil, err
@@ -161,7 +165,7 @@ func autoBufferMode(maxSize int, dir string) func(io.Reader) (buffer.Buffer, err
 		if actualSize < maxSize {
 			// Ok, the message is smaller than N. Make a MemoryBuffer and
 			// handle it in RAM.
-			log.Debugln("autobuffer: keeping the message in RAM")
+			log.Debugln("autobuffer: keeping the message in RAM (read", actualSize, "bytes, got short read)")
 			return buffer.MemoryBuffer{Slice: initial[:actualSize]}, nil
 		}
 