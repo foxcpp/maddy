ccec931b6cfa6c51cb537291f5a8234c05e18898
fox.cpp <fox.cpp@disroot.org>
2022-06-23 13:11:07 +0300

    go get -u & go mod tidy

--- internal/tls/acme/acme.go
+++ internal/tls/acme/acme.go
@@ -88,11 +88,11 @@ func (l *Loader) Init(cfg *config.Map) error {
 	})
 
 	l.cfg = certmagic.New(l.cache, certmagic.Config{
-		Storage: l.store, // not sure if it is necessary to set these twice
-		Logger:  cmLog,
+		Storage:           l.store, // not sure if it is necessary to set these twice
+		Logger:            cmLog,
 		DefaultServerName: hostname,
 	})
-	mngr := certmagic.NewACMEManager(l.cfg, certmagic.ACMEManager{
+	mngr := certmagic.NewACMEIssuer(l.cfg, certmagic.ACMEIssuer{
 		Logger: cmLog,
 		CA:     caPath,
 		Email:  email,
@@ -150,7 +150,7 @@ func (l *Loader) InstanceName() string {
 
 func init() {
 	hooks.AddHook(hooks.EventShutdown, func() {
-		certmagic.CleanUpOwnLocks(nil)
+		certmagic.CleanUpOwnLocks(context.TODO(), log.DefaultLogger.Zap())
 	})
 }
 
