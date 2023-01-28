1a603a4e534311dbe89b49d43a6495771b993ca2
Arisu Desu <arisudesu@yandex.ru>
2023-01-28 14:10:45 +0300

    Extract mapping lookup from loop

--- internal/authz/lookup.go
+++ internal/authz/lookup.go
@@ -9,26 +9,26 @@ import (
 )
 
 func AuthorizeEmailUse(ctx context.Context, username string, addrs []string, mapping module.Table) (bool, error) {
+	var validEmails []string
+	if multi, ok := mapping.(module.MultiTable); ok {
+		validEmails, err = multi.LookupMulti(ctx, username)
+		if err != nil {
+			return false, fmt.Errorf("authz: %w", err)
+		}
+	} else {
+		validEmail, ok, err := mapping.Lookup(ctx, username)
+		if err != nil {
+			return false, fmt.Errorf("authz: %w", err)
+		}
+		if ok {
+			validEmails = []string{validEmail}
+		}
+	}
+
 	for _, addr := range addrs {
 		_, domain, err := address.Split(addr)
 		if err != nil {
 			return false, fmt.Errorf("authz: %w", err)
-		}
-
-		var validEmails []string
-		if multi, ok := mapping.(module.MultiTable); ok {
-			validEmails, err = multi.LookupMulti(ctx, username)
-			if err != nil {
-				return false, fmt.Errorf("authz: %w", err)
-			}
-		} else {
-			validEmail, ok, err := mapping.Lookup(ctx, username)
-			if err != nil {
-				return false, fmt.Errorf("authz: %w", err)
-			}
-			if ok {
-				validEmails = []string{validEmail}
-			}
 		}
 
 		for _, ent := range validEmails {
